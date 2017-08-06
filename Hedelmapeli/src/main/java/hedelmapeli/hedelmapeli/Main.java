faaf31347182cdeb04538c68f69af082fb2d84d3
Maria Larionova <maria.larionova@helsinki.fi>
2017-08-06 19:01:16 +0300

    projektin pientä edistämistä

--- Hedelmapeli/src/main/java/hedelmapeli/hedelmapeli/Main.java
+++ Hedelmapeli/src/main/java/hedelmapeli/hedelmapeli/Main.java
@@ -5,6 +5,8 @@
  */
 package hedelmapeli.hedelmapeli;
 
+import java.util.Random;
+
 /**
  *
  * @author maria
@@ -15,8 +17,36 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Moi");
+        
+        
         // TODO code application logic here
+    }
+    
+    public String Arvonta(){
+        Random random = new Random();
+        int ekaLuku;
+        int tokaLuku;
+        int kolmasLuku;
+        
+        //arvotaan "hedelmät" eli luvut 1-10
+        
+        ekaLuku = 1+random.nextInt(10);     //alkuun tulee +1 jotta arvonnan jälkeen ei tulisi lukua 0
+        
+        tokaLuku = 1+random.nextInt(10);
+        
+        kolmasLuku = 1+random.nextInt(10);
+        
+        return ekaLuku + " " + tokaLuku + " " + kolmasLuku;
+        
+    }
+    public void Voitto(){
+        
+        //jos kaikki kolme lukua ovat ykkösiä, voitto on 5
+        //jos kakkosia, voitto on 10, jos kolmosia, voitto on 15 jne
+        
+        int luku = 1;
+        
+        
     }
     
 }
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Hedelmapeli
-testi
